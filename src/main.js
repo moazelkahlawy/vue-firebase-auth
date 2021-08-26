9050896b27fce47315d66772247b7076b042d206
moazelkahlawy <mezo.abdo65@gmail.com>
2021-08-26 05:14:36 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCyMDfXJWkPddHNO9rzDAaZst3eI7-VA44",
+    authDomain: "vue-auth-19d85.firebaseapp.com",
+    projectId: "vue-auth-19d85",
+    storageBucket: "vue-auth-19d85.appspot.com",
+    messagingSenderId: "73246830980",
+    appId: "1:73246830980:web:3a28bd93e70cf9f31cedd1"
+};
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome, user</h1>
+    <router-link to="/about"></router-link>
+    <br>
+    <button class="logout" @click="logout">Logout</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref, onBeforeMount } from 'vue'
+import firebase from 'firebase'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    const name = ref("")
+
+    onBeforeMount(() => {
+      const user = firebase.auth().currentUser;
+      if (user) {
+        name.value = user.email.split('@')[0];
+      }
+    })
+
+    const Logout = () => {
+      firebase
+      .auth()
+      .signOut()
+      .then(() => console.log("signed out"))
+      .catch(err => alert(err.message));
+    }
+
+    return {
+      name,
+      Logout
+    }
   }
 }
 </script>
